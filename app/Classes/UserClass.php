24b62cce2d3555442f40375f9816c7ebfaf0ffdc
hadihatoum018 <hadi.hatoum.a@hotmail.com>
2022-06-23 20:57:22 +0300

    Task

--- app/Classes/UserClass.php
+++ app/Classes/UserClass.php
@@ -17,42 +17,4 @@ class UserClass
     }
 
 
-//    public function getProfile(){
-//        try {
-//            $email = Common::AuthID();
-//            $data = $this->userManager->getProfile($email);
-//            if (isset($data) && !empty($data)){
-//                return response()->json(["status" => true, "message" => "profile get success","data"=>$data])->setStatusCode(200);
-//            }
-//        }catch (\Exception $ex){
-//            Log::info("UserClass Error", ["getProfile" => $ex->getMessage(), "line" => $ex->getLine()]);
-//            return response()->json(["status" => false, "message" => "internal server Error"])->setStatusCode(500);
-//        }
-//    }
-//    public function updateProfileInformation($email,$name){
-//        try {
-//            $update_status = $this->userManager->updateProfileInformation($email,$name);
-//            if ($update_status){
-//                return response()->json(['status' => true, 'message' => 'profile information update success'])->setStatusCode(200);
-//            }
-//            return response()->json(['status' => false, 'message' => 'Profile Up to Date'])->setStatusCode(400);
-//        } catch (\Exception $ex) {
-//            Log::info('UserClass Error', ['updateProfileHeadline' => $ex->getMessage(), 'line' => $ex->getLine()]);
-//            return response()->json(['status' => false, 'message' => 'internal server error'])->setStatusCode(500);
-//        }
-//    }
-
-//
-//    public function getUserInfo(){
-//        try {
-//            $email = Common::AuthID();
-//            $data = $this->userManager->getUserInfo($email);
-//            if (isset($data) && !empty($data)){
-//                return response()->json(["status" => true, "message" => "profile get success","data"=>$data])->setStatusCode(200);
-//            }
-//        }catch (\Exception $ex){
-//            Log::info("UserClass Error", ["getProfile" => $ex->getMessage(), "line" => $ex->getLine()]);
-//            return response()->json(["status" => false, "message" => "internal server Error"])->setStatusCode(500);
-//        }
-//    }
 }
